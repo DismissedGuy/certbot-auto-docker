4ea0f858f3b27ea31979b8c81c29825ac6880db5
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2023-04-03 18:14:52 +0200

    feat: support staging environment

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11-alpine
 
 RUN apk update && \
     apk upgrade && \
-    apk add --no-cache augeas-libs && \
+    apk add --no-cache bash augeas-libs && \
     python3 -m venv /opt/certbot && \
     /opt/certbot/bin/pip install --disable-pip-version-check --no-cache-dir -U certbot certbot-dns-cloudflare
 
--- src/renew.sh
+++ src/renew.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 log()
 {
@@ -8,8 +8,11 @@ log()
   done
 }
 
+
 echo "***** Renewing Certificate *****" | log
 
+
+## Verification
 if [ -z "${DNS_PROVIDER}" ]; then
   echo "No DNS provider (DNS_PROVIDER) specified!" | log
   exit 1
@@ -17,23 +20,34 @@ elif [ ! -f "/etc/dns-creds/${DNS_PROVIDER}.ini" ]; then
   echo "Provider ${DNS_PROVIDER} is not yet supported." | log
   exit 1
 fi
-
 if [ -z "${EMAIL}" ]; then
   echo "No Email address (EMAIL) specified!" | log
   exit 1
 fi
-
 if [ -z "${DOMAIN}" ]; then
   echo "No Domain name (DOMAIN) specified!" | log
   exit 1
 fi
 
-/opt/certbot/bin/certbot certonly \
-  --agree-tos --non-interactive --test-cert \
-  "--dns-${DNS_PROVIDER}" \
-  "--dns-${DNS_PROVIDER}-credentials" "/etc/dns-creds/${DNS_PROVIDER}.ini" \
-  "--dns-${DNS_PROVIDER}-propagation-seconds" 15 \
-  -m "${EMAIL}" \
-  -d "${DOMAIN},*.${DOMAIN}" \
-  --preferred-challenges dns-01 2>&1 | log
+
+## Arguments building
+certbot_args=(
+  "--agree-tos" "--non-interactive"
+  "--preferred-challenges" "dns-01"
+  
+  "--dns-${DNS_PROVIDER}"
+  "--dns-${DNS_PROVIDER}-credentials" "/etc/dns-creds/${DNS_PROVIDER}.ini"
+  
+  "-m" "${EMAIL}"
+  "-d" "${DOMAIN},*.${DOMAIN}"
+)
+if [ ! -z "${STAGING}" ] && [ "${STAGING,,}" != "false" ]; then
+  echo "** Using Staging environment **" | log
+  certbot_args+=("--test-cert")
+fi
+
+echo "** Running with options: ${certbot_args[*]}"
+
+## Actual renewal
+/opt/certbot/bin/certbot certonly "${certbot_args[*]}" 2>&1 | log
 echo
