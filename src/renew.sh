86bb8f3a018e2e1a27a4a9014d45bf06e153efa2
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2023-04-20 19:40:18 +0200

    Add support for multiple domains

--- src/renew.sh
+++ src/renew.sh
@@ -24,37 +24,44 @@ if [ -z "${EMAIL}" ]; then
   echo "No Email address (EMAIL) specified!" | log
   exit 1
 fi
-if [ -z "${DOMAIN}" ]; then
-  echo "No Domain name (DOMAIN) specified!" | log
+if [ -z "${DOMAINS}" ]; then
+  echo "No Domain names (DOMAINS) specified!" | log
   exit 1
 fi
 
 
-## Arguments building
-certbot_args=(
-  "--agree-tos" "--non-interactive"
-  "--preferred-challenges" "dns-01"
-  
-  "--dns-${DNS_PROVIDER}"
-  "--dns-${DNS_PROVIDER}-credentials" "/etc/dns-creds/${DNS_PROVIDER}.ini"
-  
-  "-m" "${EMAIL}"
-  "-d" "${DOMAIN},*.${DOMAIN}"
-)
-if [ ! -z "${STAGING}" ] && [ "${STAGING,,}" != "false" ]; then
-  echo "** Using Staging environment **" | log
-  certbot_args+=("--test-cert")
-fi
-
 export IFS=";"
-for pair in $EXTRA_ARGS; do
-  opt_str="--${pair//=/ }"
-  certbot_args+=("${opt_str,,}")
+for domain in $DOMAINS; do
+  unset IFS
+  echo "** Requesting certificate for $domain" | log
+
+  ## Arguments building
+  certbot_args=(
+    "--agree-tos" "--non-interactive"
+    "--preferred-challenges" "dns-01"
+
+    "--dns-${DNS_PROVIDER}"
+    "--dns-${DNS_PROVIDER}-credentials" "/etc/dns-creds/${DNS_PROVIDER}.ini"
+
+    "-m" "${EMAIL}"
+    "-d" "${domain},*.${domain}"
+  )
+
+  if [ ! -z "${STAGING}" ] && [ "${STAGING,,}" != "false" ]; then
+    echo "** Using Staging environment **" | log
+    certbot_args+=("--test-cert")
+  fi
+
+  export IFS=";"
+  for pair in $EXTRA_ARGS; do
+    opt_str="--${pair//=/ }"
+    certbot_args+=("${opt_str,,}")
+  done
+  unset IFS
+
+  echo "** Running with options: ${certbot_args[*]}" | log
+
+  ## Actual renewal
+  /opt/certbot/bin/certbot certonly ${certbot_args[*]} 2>&1 | log
+  echo
 done
-unset IFS
-
-echo "** Running with options: ${certbot_args[*]}" | log
-
-## Actual renewal
-/opt/certbot/bin/certbot certonly ${certbot_args[*]} 2>&1 | log
-echo
